0c5ef1971b3029c940acaf2719658f49ac5f2358
Adsidera <annamaria.costalonga@gmail.com>
2019-11-21 11:07:10 +0100

    removed unnecessary double caching

--- app/services/geocode_fetcher.rb
+++ app/services/geocode_fetcher.rb
@@ -3,16 +3,12 @@
 class GeocodeFetcher
   URL = 'https://eu1.locationiq.com/v1/search.php'
 
+  attr_reader :response
+
   def initialize(query)
     @data = { 'key': ENV['GEO_TOKEN'], 'q': query, format: 'json' }
     @response = Rails.cache.fetch(query) do
       Faraday.get(URL, @data)
-    end
-  end
-
-  def response
-    Rails.cache.fetch(@response) do
-      @response
     end
   end
 end
