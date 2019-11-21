e067bafe78d5a48df62e6547783568e96c4ae5a6
Adsidera <annamaria.costalonga@gmail.com>
2019-11-21 10:49:12 +0100

    fixed cache duration

--- app/services/geocode_fetcher.rb
+++ app/services/geocode_fetcher.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
-require 'colorize'
 class GeocodeFetcher
   URL = 'https://eu1.locationiq.com/v1/search.php'
 
   def initialize(query)
     @data = { 'key': ENV['GEO_TOKEN'], 'q': query, format: 'json' }
-    @response = Rails.cache.fetch("response", expires_in: 30.minutes) do
+    @response = Rails.cache.fetch(query) do
       Faraday.get(URL, @data)
     end
-    Rails.logger.info "Cached response".yellow
   end
 
   def response
-    Rails.cache.fetch(@response, expires_in: 30.minutes) do
+    Rails.cache.fetch(@response) do
       @response
     end
   end
